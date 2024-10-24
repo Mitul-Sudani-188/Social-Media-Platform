a1df8312b4fdba19d54f6f41a8d0183dbcda6693
Harsh Bosamiya <202201243@daiict.ac.in>
2024-10-24 17:53:24 +0530

    database connection

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,10 +1,12 @@
 import express, { urlencoded } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
-
-
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config({});
 const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (_,res)=>{
     return res.status(200).json({
@@ -26,8 +28,9 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-const PORT = 8000;
 
-app.listen(PORT, () => {
+
+app.listen(PORT, ()=>{
+    connectDB();
     console.log(`Server listen at port ${PORT}`);
-});
+})
